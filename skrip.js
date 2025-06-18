327ded50e8c849bfcc6630cd98387cc8c79530d4
rifkyprtama0017 <rifkycc20145@gmail.com>
2025-06-18 13:40:00 +0700

    coba aja

---
+++ skrip.js
@@ -0,0 +1,30 @@
+a="Di situs web saya! Saat ini, web ini masih dalam tahap pengembangan dan penyempurnaan untuk memberikan pengalaman terbaik bagi Anda. Terima kasih banyak telah berkunjung dan meluangkan waktu di sini. Masukan dan dukungan Anda sangat berarti bagi perkembangan web ini ke depannya.";
+document.getElementById("welcome").innerHTML=a;
+
+b5="Nama saya Rifky Pratama Wahyu Ramadhan biasa dipanggil Rifky"
+document.getElementById("item5").innerHTML=b5;
+
+b6="Hobi saya adalah naik kereta api dan suka sekali naik kereta ataupun memotret kereta api"
+document.getElementById("item6").innerHTML=b6;
+
+b7="Saya lahir di Cilandak tepatnya di kota Jakarta bagian Selatan, umur saya 22 tahun dihitung sejak tahun 2003"
+document.getElementById("item7").innerHTML=b7;
+
+b8="Saya tinggal di Jakarta Selatan yakni tepatnya di Cilandak, tempat dimana saya lahir"
+document.getElementById("item8").innerHTML=b8;
+
+a1="Nama"
+document.getElementById("itemA").innerHTML=a1;
+b1="Hobi"
+document.getElementById("itemB").innerHTML=b1;
+c1="Tempat Lahir"
+document.getElementById("itemC").innerHTML=c1;
+d1="Tinggal"
+document.getElementById("itemD").innerHTML=d1;
+
+b9="Verifikasi aman,anda telah menjadi layanan anggota kami"
+document.getElementById("a1").innerHTML=b9;
+b10="anda belum terdaftar menjadi anggota kami, cek kembali alasan pengajuan ada di tolak"
+document.getElementById("a2").innerHTML=b10;
+b11="proses pengajuan telah diterima dan sedang proses pengecekan"
+document.getElementById("a3").innerHTML=b11;
